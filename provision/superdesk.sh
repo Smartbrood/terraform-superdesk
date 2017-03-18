3ffc033428ce67dbd31a8a96bb0d37f408d21ef8
Andrey N. Petrov <andreynpetrov@gmail.com>
2017-03-18 20:52:47 +0300

    intermediate auto

--- provision/superdesk.sh
+++ provision/superdesk.sh
@@ -5,5 +5,5 @@ if [ -d /opt/superdesk/server ] ; then
 	echo "Superdesk already installed. Skip installation step."
 else
 	echo "Start Superdesk installation"
-	#curl -s https://raw.githubusercontent.com/superdesk/fireq/master/files/superdesk/install | sudo bash && echo "Superdesk is installed."
+	curl -s https://raw.githubusercontent.com/superdesk/fireq/master/files/superdesk/install | sudo bash && echo "Superdesk is installed."
 fi
